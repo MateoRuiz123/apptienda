2f4110b3bacfd37b7a15bc67f62964e099cef4c1
MateoRuiz123 <mrivera736@soy.sena.edu.co>
2023-08-15 08:38:03 -0500

    estilo de login

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         '/vendedores': (BuildContext context) => const Vendedores(),
         '/power': (BuildContext context) => const Power(),
-        '/loginPage': (BuildContext context) => LoginPage(),
+        '/loginPage': (BuildContext context) => const LoginPage(),
         '/perfil': (BuildContext context) => Perfil(),
       },
     );
@@ -41,8 +41,10 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class UserData {
@@ -62,7 +64,7 @@ class _LoginPageState extends State<LoginPage> {
 
   Future<void> _login() async {
     final response = await http.post(
-      Uri.parse('http://10.170.83.30/tienda/login.php'),
+      Uri.parse('http://10.170.82.219/tienda/login.php'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -74,17 +76,19 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
 
-    print(response.body);
+    // print(response.body);
 
     var datauser = json.decode(response.body);
-    print(datauser); // Verifica la respuesta del servidor en la consola
+    // print(datauser); // Verifica la respuesta del servidor en la consola
 
     if (datauser != null && datauser.containsKey('nivel')) {
       final role =
           datauser['nivel']; // Obtén el rol del usuario desde los datos
       if (role == 'admin') {
+        // ignore: use_build_context_synchronously
         Navigator.pushReplacementNamed(context, '/power');
       } else if (role == 'ventas') {
+        // ignore: use_build_context_synchronously
         Navigator.pushReplacementNamed(context, '/vendedores');
       } else {
         // Manejar otros roles o mostrar un mensaje de error
@@ -101,22 +105,155 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(title: const Text('Login')),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(16.0),
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         TextField(
+    //             controller: _usernameController,
+    //             decoration: const InputDecoration(labelText: 'Username')),
+    //         TextField(
+    //             controller: _passwordController,
+    //             decoration: const InputDecoration(labelText: 'Password')),
+    //         const SizedBox(height: 20),
+    //         ElevatedButton(onPressed: _login, child: const Text('Login')),
+    //       ],
+    //     ),
+    //   ),
+    // );
+
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username')),
-            TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password')),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: _login, child: Text('Login')),
-          ],
+      resizeToAvoidBottomInset: false,
+      body: Form(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/digital.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 77.0),
+                // ignore: sort_child_properties_last
+                child: const CircleAvatar(
+                  backgroundColor: Color(0x0f81f7f3),
+                  child: Image(
+                    width: 135,
+                    height: 135,
+                    image: AssetImage('assets/images/avatar.png'),
+                  ),
+                ),
+                width: 170.0,
+                height: 170.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.only(top: 93),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      padding: const EdgeInsets.only(
+                          top: 4, left: 16, right: 16, bottom: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5,
+                              offset: Offset(0, 2))
+                        ],
+                      ),
+                      child: TextFormField(
+                        controller: _usernameController,
+                        decoration: const InputDecoration(
+                          icon: Icon(
+                            Icons.person,
+                            color: Colors.black,
+                          ),
+                          hintText: 'Username',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      height: 50,
+                      margin: const EdgeInsets.only(top: 32),
+                      padding: const EdgeInsets.only(
+                          top: 4, left: 16, right: 16, bottom: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 5,
+                              offset: Offset(0, 2))
+                        ],
+                      ),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.black,
+                          ),
+                          hintText: 'Password',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          top: 6,
+                          right: 32,
+                        ),
+                        child: Text(
+                          'Forgot Password ?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        _login();
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      msg,
+                      style: const TextStyle(fontSize: 25, color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
